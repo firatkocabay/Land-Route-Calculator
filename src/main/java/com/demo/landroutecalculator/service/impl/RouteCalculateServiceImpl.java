11d3d525261772f1948233e8f0b1b996f8f610ac
fkocabay <fkocabay@partner.innova.com.tr>
2022-08-14 18:54:05 +0300

    refactor code for single responsibility

--- src/main/java/com/demo/landroutecalculator/service/impl/RouteCalculateServiceImpl.java
+++ src/main/java/com/demo/landroutecalculator/service/impl/RouteCalculateServiceImpl.java
@@ -24,25 +24,43 @@ public class RouteCalculateServiceImpl implements RouteCalculateService {
     public LandRouteCalculateDto calculateLandRoute(String originCountry, String destinationCountry) {
         LandRouteCalculateDto landRouteCalculateDto = new LandRouteCalculateDto();
         List<String> originCountryBorders = countryInformationService.getRelatedCountryBorders(originCountry);
-
         List<String> route = new ArrayList<>(new LinkedHashSet<>());
 
-        if (originCountryBorders.isEmpty() || originCountry.equals(destinationCountry)) {
-            landRouteCalculateDto.setRoute(route);
+        if (checkCountryBorderAndIsSameCountry(originCountry, destinationCountry, landRouteCalculateDto, originCountryBorders, route))
             return landRouteCalculateDto;
-        }
 
         route.add(originCountry);
 
+        checkNeighborCountries(destinationCountry, landRouteCalculateDto, originCountryBorders, route);
+        if (route.contains(destinationCountry)) return landRouteCalculateDto;
+
+        List<String> destinationCountryBorders = countryInformationService.getRelatedCountryBorders(destinationCountry);
+        calculateBasicLandRoute(destinationCountry, landRouteCalculateDto, originCountryBorders, route, destinationCountryBorders);
+        if (route.contains(destinationCountry)) return landRouteCalculateDto;
+
+        calculateRouteUsingLandBorders(route, destinationCountry, originCountryBorders, destinationCountryBorders, landRouteCalculateDto);
+
+        return landRouteCalculateDto;
+    }
+
+    private boolean checkCountryBorderAndIsSameCountry(String originCountry, String destinationCountry, LandRouteCalculateDto landRouteCalculateDto, List<String> originCountryBorders, List<String> route) {
+        if (originCountryBorders.isEmpty() || originCountry.equals(destinationCountry)) {
+            landRouteCalculateDto.setRoute(route);
+            return true;
+        }
+        return false;
+    }
+
+    private LandRouteCalculateDto checkNeighborCountries(String destinationCountry, LandRouteCalculateDto landRouteCalculateDto, List<String> originCountryBorders, List<String> route) {
         if (originCountryBorders.contains(destinationCountry)) {
             route.add(destinationCountry);
             landRouteCalculateDto.setRoute(route);
             return landRouteCalculateDto;
         }
+        return landRouteCalculateDto;
+    }
 
-
-        List<String> destinationCountryBorders = countryInformationService.getRelatedCountryBorders(destinationCountry);
-
+    private LandRouteCalculateDto calculateBasicLandRoute(String destinationCountry, LandRouteCalculateDto landRouteCalculateDto, List<String> originCountryBorders, List<String> route, List<String> destinationCountryBorders) {
         originCountryBorders.forEach(originBorder -> destinationCountryBorders.forEach(destinationBorder -> {
             if (originBorder.equals(destinationBorder) && !route.contains(destinationCountry)) {
                 log.info("Contains neighbor border land country of {}", originBorder);
@@ -55,16 +73,13 @@ public class RouteCalculateServiceImpl implements RouteCalculateService {
         if (route.contains(destinationCountry)) {
             return landRouteCalculateDto;
         }
-
-        calculateRouteUsingLandBorders(route, originCountry, destinationCountry, originCountryBorders, landRouteCalculateDto);
-
         return landRouteCalculateDto;
     }
 
     private LandRouteCalculateDto calculateRouteUsingLandBorders(List<String> route,
-                                                                 String originCountry,
                                                                  String destinationCountry,
                                                                  List<String> originCountryBorders,
+                                                                 List<String> destinationCountryBorders,
                                                                  LandRouteCalculateDto landRouteCalculateDto) {
         log.info("Route List 1: {}", route);
 
@@ -82,7 +97,7 @@ public class RouteCalculateServiceImpl implements RouteCalculateService {
                 route.remove(border);
                 continue;
             }
-            this.calculateRouteUsingLandBorders(route, originCountry, destinationCountry, neighborCountryLandBorders, landRouteCalculateDto);
+            this.calculateRouteUsingLandBorders(route, destinationCountry, neighborCountryLandBorders, destinationCountryBorders, landRouteCalculateDto);
         }
 
         return landRouteCalculateDto;
